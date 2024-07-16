906235e0dae24f83971f1ecef15b7c022f259a69
paresiqbal <thecrowdel@gmail.com>
2024-07-16 22:54:38 +0700

    Update

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -2,6 +2,11 @@
 
 // prevent direct access
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstname = $_POST["firstname"];
-    asdasdasds
+    $firstName = htmlspecialchars($_POST["firstname"]);
+    $lastName = htmlspecialchars($_POST["lastname"]);
+    $favGames = htmlspecialchars($_POST["favoritegame"]);
+
+    echo "First Name: $firstName <br>";
+    echo "Last Name: $lastName <br>";
+    echo "Favorite Game: $favGames <br>";
 }
--- index.php
+++ index.php
@@ -24,9 +24,6 @@
                 <option value="roblox">Roblox</option>
                 <option value="amongus">Among Us</option>
             </select>
-            <h1>Form</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat aperiam, quo officia ratione eos, ipsum et qui quas ipsam deleniti deserunt. Rerum ipsum labore exercitationem explicabo, fuga earum cupiditate quasi iste quam vero excepturi saepe pariatur, officiis laborum ea magni.</p>
-
             <button type="submit" name="submit">Submit</button>
         </form>
     </main>
