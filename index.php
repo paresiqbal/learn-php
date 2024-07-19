6bfcade1f10c88407bca6cd50beaf7bc15575d64
paresiqbal <thecrowdel@gmail.com>
2024-07-19 20:07:56 +0700

    Create form for calculator

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
 </head>
 
 <body>
-
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+        <input type="number" name="numb1" placeholder="first number">
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="sub">-</option>
+            <option value="mul">*</option>
+            <option value="div">/</option>
+        </select>
+        <input type="number" name="numb2" placeholder="second number">
+        <button>calculatted</button>
+    </form>
 </body>
 
 </html>
