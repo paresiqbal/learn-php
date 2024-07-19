2be3d754b91cad8d41f3ffbe07bd9520fe9619ca
paresiqbal <thecrowdel@gmail.com>
2024-07-19 20:27:25 +0700

    Create variable to catch data from Form

--- index.php
+++ index.php
@@ -9,16 +9,25 @@
 
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
-        <input type="number" name="numb1" placeholder="first number">
+        <input type="number" name="numb1" placeholder="first number" require>
         <select name="operator">
             <option value="add">+</option>
             <option value="sub">-</option>
             <option value="mul">*</option>
             <option value="div">/</option>
         </select>
-        <input type="number" name="numb2" placeholder="second number">
+        <input type="number" name="numb2" placeholder="second number" require>
         <button>calculatted</button>
     </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // catch data from form 
+        $numb1 = filter_input(INPUT_POST, "numb1", FILTER_SANITIZE_NUMBER_FLOAT);
+        $numb2 = filter_input(INPUT_POST, "numb2", FILTER_SANITIZE_NUMBER_FLOAT);
+        $operator = htmlspecialchars($_POST["operator"]);
+    }
+    ?>
 </body>
 
 </html>
