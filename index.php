32f0aa714f9bfc19aa478603414127cf599ae90f
paresiqbal <thecrowdel@gmail.com>
2024-07-23 23:50:45 +0700

    chore: Refactor formhandler and index.php, improve form submission handling

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <h1>Signup</h1>
     <form action="includes/formhandler.inc.php" method="post">
         <input type="text" name="username" placeholder="Username">
-        <input type="text" name="email" placeholder="Email">
         <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
         <button>Sign Up</button>
     </form>
 </body>
