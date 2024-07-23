435c56cdae3e4f85ae6c3d33e8f24d090b603fcc
paresiqbal <thecrowdel@gmail.com>
2024-07-23 23:35:05 +0700

    Create form

--- index.php
+++ index.php
@@ -8,15 +8,13 @@
 </head>
 
 <body>
-
-    <?php
-
-    ?>
+    <h1>Signup</h1>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="email" placeholder="Email">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Sign Up</button>
+    </form>
 </body>
 
 </html>
-INSERT INTO users (username, pwd, email) VALUES (
-'Pares',
-'password123',
-'pares@email.com'
-);
