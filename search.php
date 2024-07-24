ae28de671201ba9bf29633dbc8fec5803a51761b
paresiqbal <thecrowdel@gmail.com>
2024-07-24 13:52:25 +0700

    chore: Refactor search.php, implement user search functionality

--- search.php
+++ search.php
@@ -1,3 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userSearch = $_POST["usersearch"];
+
+    try {
+        require_once "includes/dbh.inc.php";
+
+        $query = "SELECT * FROM comments WHERE username = :usersearch;";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":usersearch", $userSearch);
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $pdo = null;
+        $stmt = null;
+    } catch (PDOException $e) {
+        die("Error: " . $e->getMessage());
+    }
+} else {
+    header("Location: ../index.php");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +33,20 @@
 </head>
 
 <body>
-
+    <h3>Result</h3>
+    <?php
+    if (empty($results)) {
+        echo "No results found.";
+    } else {
+        foreach ($results as $row) {
+            echo "<div>";
+            echo htmlspecialchars($row["username"]);
+            echo htmlspecialchars($row["comment_text"]);
+            echo htmlspecialchars($row["created_at"]);
+            echo "</div>";
+        }
+    }
+    ?>
 </body>
 
 </html>
