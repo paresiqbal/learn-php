494566e790b9dd349f6d79d79f949ef7c5c96042
paresiqbal <thecrowdel@gmail.com>
2024-07-26 22:56:24 +0700

    asdasd

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once "config.php";
 </head>
 
 <body>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, quod eius quae consequatur aperiam voluptatum debitis quia quisquam ipsam voluptates veritatis, modi officiis explicabo fuga unde! Consequatur at reprehenderit facilis dolorum, saepe deleniti, rem odit vel totam voluptates eligendi quo nemo beatae temporibus quaerat suscipit numquam in! Nobis animi fugit eos optio numquam ratione, impedit sapiente, nostrum inventore quasi doloribus enim reiciendis odit magnam exercitationem dolor est. Numquam debitis ad excepturi maxime placeat illum cum quidem! Dicta, impedit! Qui saepe culpa aut nam iusto nisi nostrum error tempora aspernatur accusamus maiores commodi, et tenetur. Beatae ipsam quibusdam tenetur eum nobis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, consectetur?</p>
 </body>
 
 </html>
