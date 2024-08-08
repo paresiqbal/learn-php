fc204ca1b9dc6819cfad433cf921bede113302bf
paresiqbal <thecrowdel@gmail.com>
2024-08-08 23:37:23 +0700

    Refactor signup form validation functions for improved code readability and maintainability

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -11,16 +11,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         require_once "signup_contr.inc.php";
 
         // handling error
+        $errors = [];
+
         if (is_input_emty($username, $email, $pwd)) {
+            $errors["empty input"] = "Fill all fields!";
         }
 
         if (is_email_invalid($email)) {
+            $errors["invalid email"] = "Invalid email!";
         }
 
         if (is_username_taken($pdo, $username)) {
+            $errors["username taken"] = "Username already taken!";
         }
 
         if (is_email_registered($pdo, $email)) {
+            $errors["email registered"] = "Email already registered!";
+        }
+
+        if ($errors) {
+            $_SESSION["errors"] = $errors;
         }
     } catch (PDOException $e) {
         die("Query Failed: " . $e->getMessage());
