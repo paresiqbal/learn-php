b5f571999eebbe5928598ceb4098cb7bc80169c7
paresiqbal <thecrowdel@gmail.com>
2024-08-08 22:04:40 +0700

    Fix typo

--- includes/config_session.icn.php
+++ includes/config_session.icn.php
@@ -3,7 +3,7 @@
 ini_set("session.use_only_cookies", 1);
 ini_set("session.use_strict_mode", 1);
 
-session_set_coockie_params([
+session_set_cookie_params([
     "lifetime" => 1800,
     "domain" => "localhost",
     "secure" => true,
@@ -14,8 +14,16 @@ session_set_coockie_params([
 session_start();
 
 if (!isset($_SESSION["last_regenerate"])) {
-    session_regenerate_id();
-    $_SESSION["last_regenerate"] = time();
+    regenerate_session_id();
 } else {
     $interval = 60 * 30;
+    if (time() - $_SESSION["last_regenerate"] > $interval) {
+        regenerate_session_id();
+    }
+}
+
+function regenerate_session_id()
+{
+    session_regenerate_id(true);
+    $_SESSION["last_regenerate"] = time();
 }
