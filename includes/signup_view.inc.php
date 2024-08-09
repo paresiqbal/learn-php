0fae26ec8d5cf923c40c9551f651f0cd706416cc
paresiqbal <thecrowdel@gmail.com>
2024-08-09 20:43:26 +0700

    Refactor signup form validation functions for improved code readability and maintainability

--- includes/signup_view.inc.php
+++ includes/signup_view.inc.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 function check_signup_errors()
 {
-    if (isset($_SESSION["errors"])) {
+    if (isset($_SESSION["errors_signup"])) {
         $errors = $_SESSION["errors_signup"];
 
         echo "<br>";
@@ -14,5 +14,8 @@ function check_signup_errors()
         }
 
         unset($_SESSION["errors_signup"]);
+    } elseif (isset($_GET["signup"]) && $_GET["signup"] === "success") {
+        echo "<br>";
+        echo "<p style='color: green;'>Signup successful!</p>";
     }
 }
