1173733b089c8f541e52bacb33ceddc1b88e0ce3
paresiqbal <thecrowdel@gmail.com>
2024-08-26 23:11:25 +0700

    asdasdsad

--- index.php
+++ index.php
@@ -13,26 +13,7 @@ require_once "includes/signup_view.inc.php";
 </head>
 
 <body>
-    <h3>Login</h3>
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <button>Login</button>
-    </form>
 
-    <h3>Signup</h3>
-    <form action="includes/signup.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="text" name="email" placeholder="Email">
-        <input type="password" name="pwd" placeholder="Password">
-        <button type="submit">Signup</button>
-    </form>
-    <h1>Lorem ipsum dolor sit amet.asdasd</h1>asdasdasd
-
-
-    <?php
-    check_signup_errors();
-    ?>
 </body>
 
 </html>
